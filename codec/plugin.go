bbaa0e8985970af9de9372efaa8a18e025ac7e2b
rookie-xy <meng1985626@163.com>
2017-07-26 21:37:22 +0800

    update

---
+++ codec/plugin.go
@@ -0,0 +1,41 @@
+package codec
+
+import (
+    "fmt"
+    "errors"
+    "github.com/rookie-xy/worker/src/plugin"
+    "github.com/rookie-xy/worker/src/plugin/codec"
+    "github.com/rookie-xy/worker/src/register"
+
+  _ "github.com/rookie-xy/plugins/codec/yaml"
+)
+
+type codecPlugin struct {
+    name    string
+    factory codec.Factory
+}
+
+const Name = "codec"
+
+func Plugin(name string, f codec.Factory) map[string][]interface{} {
+     return plugin.Make(name, codecPlugin{name, f})
+}
+
+func init() {
+    plugin.MustRegisterLoader(Name, func(ifc interface{}) (err error) {
+        b, ok := ifc.(codecPlugin)
+        if !ok {
+            return errors.New("plugin does not match output codec plugin type")
+        }
+
+        defer func() {
+            if msg := recover(); msg != nil {
+                err = fmt.Errorf("%s", msg)
+	    }
+        }()
+
+        register.Codec(b.name, b.factory)
+
+        return
+    })
+}
--- codec/yaml/yaml.go
+++ codec/yaml/yaml.go
@@ -1,27 +1,48 @@
-package codec
-
+package yaml
 
 import (
     yml "gopkg.in/yaml.v2"
-    "github.com/rookie-xy/worker/src/prototype"
-    "github.com/rookie-xy/worker/src/state"
-    "github.com/rookie-xy/worker/src/register"
     "github.com/rookie-xy/worker/src/plugin/codec"
-    "fmt"
+    "github.com/rookie-xy/worker/src/prototype"
+    "github.com/rookie-xy/worker/src/register"
 )
+
+const Name = "yaml"
 
 type Yaml struct {
     name string
 }
 
-func NewYaml() *Yaml {
+func New() *Yaml {
     return &Yaml{}
 }
 
-var yaml = &Yaml{
-    name: "yaml",
+func (r *Yaml) Encode(in prototype.Object) (prototype.Object, error) {
+    out, error := yml.Marshal(in);
+    if error != nil {
+        return nil, error
+    }
+
+    return out, nil
 }
 
+func (r *Yaml) Decode(in []byte) (prototype.Object, error) {
+    var out interface{}
+
+    if e := yml.Unmarshal(in, &out); e != nil {
+        return nil, e
+    }
+
+    return out, nil
+}
+
+func init() {
+    register.Codec(Name, func(cfg *codec.Config) (codec.Codec, error) {
+        return New(), nil
+    })
+}
+
+/*
 func (r *Yaml) Clone() codec.Codec {
     yaml := NewYaml()
     yaml.name = "yaml"
@@ -65,3 +86,4 @@ func (r *Yaml) Type(name string) int {
 func init() {
     register.Codecs("yml", yaml)
 }
+*/
