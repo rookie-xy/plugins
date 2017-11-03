12242f6a2a9498308ee421b17e4a542986a0f8ec
Meng Shi <mengshi@meicai.cn>
2017-11-03 19:48:26 +0800

    update

--- client/elasticsearch/elasticsearch.go
+++ client/elasticsearch/elasticsearch.go
@@ -7,6 +7,7 @@ import (
     "github.com/rookie-xy/hubble/register"
     "github.com/rookie-xy/hubble/proxy"
     "github.com/rookie-xy/hubble/types"
+    "fmt"
 )
 
 const Namespace = "plugin.client.elasticsearch"
@@ -16,35 +17,20 @@ type elasticsearch struct {
 }
 
 func open(l log.Log, v types.Value) (proxy.Forward, error) {
+    fmt.Println("iniiiiiiiiiiiiiiiiiiiiiiiiiiielasticsearch")
     return &elasticsearch{
         Log: l,
     }, nil
 }
 
 func (r *elasticsearch) Sender(e event.Event) error {
+    fmt.Println("elasticsearchhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhxxxxxxxxxxxxxxxxxxxxxxxxxxx")
     return nil
 }
 
 func (r *elasticsearch) Close() int {
     return state.Ok
 }
-/*
-func (r *elasticsearch) Post(e event.Event) int {
-    return state.Ok
-}
-
-func (r *elasticsearch) Delete(e event.Event) int {
-    return state.Ok
-}
-
-func (r *elasticsearch) Put(e event.Event) int {
-    return state.Ok
-}
-
-func (r *elasticsearch) Get(e event.Event) types.Object {
-    return state.Ok
-}
-*/
 
 func init() {
     register.Client(Namespace, open)
